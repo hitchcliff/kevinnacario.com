a540f747950eeaa0c08c29f753a1050fa9bd711a
Kevin Nacario <hitchcliff1@gmail.com>
2020-12-24 11:44:12 +0800

    updated layout

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,27 +1,19 @@
 enum Types {
-  Submit = "submit",
+  Submit = 'submit',
 }
 interface ButtonProps {
   submit?: boolean;
   handleClick?: (e: boolean) => void;
   children: any;
 }
-export default function CustomButton({
-  submit,
-  handleClick,
-  children,
-}: ButtonProps) {
+export default function CustomButton({ submit, handleClick, children }: ButtonProps) {
   function onClick() {
     if (!handleClick) return;
     handleClick(true);
   }
 
   return (
-    <button
-      onClick={onClick}
-      type={submit ? Types.Submit : undefined}
-      className="block py-2 px-4 btn"
-    >
+    <button onClick={onClick} type={submit ? Types.Submit : undefined} className="block py-2 px-4 btn">
       {children}
     </button>
   );
