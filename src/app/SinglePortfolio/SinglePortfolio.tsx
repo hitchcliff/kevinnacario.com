65cb77346656685b9d4f3bbd0c12e4d9e331c0db
Kevin Nacario <hitchcliff1@gmail.com>
2020-12-27 20:28:01 +0800

    finalize layout

--- src/app/SinglePortfolio/SinglePortfolio.tsx
+++ src/app/SinglePortfolio/SinglePortfolio.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { CustomButton } from '../../components';
+import DesktopMockup from '../../assets/images/sample.png';
+import MobileMockup from '../../assets/images/sample_mobile.png';
+import TabletMockup from '../../assets/images/sample_tablet.png';
+import SampleImage from '../../assets/images/lpado.jpg';
+import { motion } from 'framer-motion';
 
 interface SinglePortfolioProps {
   match: { params: { id: string } };
 }
 
 export default function SinglePortfolio({ match }: SinglePortfolioProps) {
-  console.log(match.params.id);
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row flex-wrap items-center px:10 gap-10 lg:px-56 lg:flex-nowrap min-h-screen bg-white">
+      <div className="flex flex-row flex-wrap items-center px:10 gap-10 lg:px-56 lg:flex-nowrap bg-white">
         <div className="w-1/2">
-          <h1 className="text-5xl font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dolore?</h1>
+          <h1 className="text-5xl font-light">Lpado</h1>
           <div className="mt-10">
             <CustomButton>Visit site</CustomButton>
           </div>
         </div>
-        <div>Image here</div>
+        <div className="w-full h-full">
+          <img className="h-full w-full object-cover" src={SampleImage} alt="" />
+        </div>
       </div>
       <div className="flex flex-row gap-12 bg-gray-100 py-24 h-1/2  px:10 w-full lg:px-56" style={{ height: '40vh' }}>
         <div>
@@ -41,12 +47,19 @@ export default function SinglePortfolio({ match }: SinglePortfolioProps) {
           </div>
         </div>
       </div>
-      <div className="px-10 pb-24 -mt-20 md:-mt-30 lg:-mt-40 lg:px-56">
-        <div className="relative flex flex-row flex-wrap lg:flex-nowrap bg-gray-100" style={{ height: '60vh' }}>
-          <div className="w-full h-full bg-white shadow-lg z-0">Desktop mockup</div>
-          <div className="w-1/4 h-full bg-white z-10 shadow-xl lg:absolute lg:top-36 lg:right-36">mobile mockup</div>
+      <div className="pb-24 -mt-20 md:-mt-30 lg:-mt-40">
+        <div className="flex flex-row flex-wrap items-center px-10 gap-5 lg:flex-nowrap " style={{ height: '50vh' }}>
+          <motion.div className="w-1/2 h-3/4 bg-white shadow-xl z-0 overflow-x-hidden overflow-y-scroll vertical-scrollbar">
+            <img className="object-cover" src={TabletMockup} alt="sample.jpg" />
+          </motion.div>
+          <motion.div className="w-full h-full bg-white shadow-xl z-0 overflow-x-hidden overflow-y-scroll vertical-scrollbar">
+            <img className="object-cover" src={DesktopMockup} alt="sample.jpg" />
+          </motion.div>
+          <motion.div className="w-1/4 h-5/6 bg-white z-10 overflow-hidden shadow-xl overflow-x-hidden overflow-y-scroll vertical-scrollbar">
+            <img className="object-cover" src={MobileMockup} alt="sample" />
+          </motion.div>
         </div>
-        <div className="mt-10 lg:w-1/2 lg:mr-auto">
+        <div className="mt-10 px-10 lg:px-56 lg:mr-auto">
           <h2>Concept</h2>
           <p className="mt-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis tempora veniam illo vitae, quod aspernatur. Mollitia veritatis quia, fugiat
