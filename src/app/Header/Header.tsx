c483d02226f2fd151d6e2946a96395b8e41c9b78
Kevin Nacario <hitchcliff1@gmail.com>
2020-12-28 15:23:21 +0800

    adjusted header responsiveness

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -6,7 +6,7 @@ import { slideDownFrameAnim } from '../../components/animation/slide';
 
 export default function Header() {
   return (
-    <motion.nav {...slideDownFrameAnim} className="px-10 shadow-sm py-5 sticky top-0 bg-white z-20 md:px-56">
+    <motion.nav {...slideDownFrameAnim} className="px-10 shadow-sm py-5 sticky top-0 bg-white z-20 lg:px-56">
       <Large />
       <Small />
     </motion.nav>
