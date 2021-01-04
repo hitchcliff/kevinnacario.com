6e77d04c342a3c95bedeac82bdf2db2868a93eac
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-05 00:19:20 +0800

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Footer, Header } from './app/index';
-import { Loading } from './components';
 import AppRoutes from './routes/App.routes';
 import './styles/main.css';
 
