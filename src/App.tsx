478bba813a0535673bbddeff4c86b4e06fe7c743
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-05 16:02:07 +0800

    test 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Footer, Header } from './app/index';
 import AppRoutes from './routes/App.routes';
 import './styles/main.css';
+import gsap from 'gsap';
+import { CSSPlugin } from 'gsap/CSSPlugin';
 
 function App() {
+  gsap.registerPlugin(CSSPlugin);
+
   return (
     <Router>
       <Header />
